ccc9592ac608ef3e9d8563e84bab42535bd7868f
Chris Sammis <csammis@gmail.com>
2013-02-12 11:12:47 -0600

    Test copy constructor

--- src/CSammisRun.Imaging.Test/OneBppImageTest.cs
+++ src/CSammisRun.Imaging.Test/OneBppImageTest.cs
@@ -13,27 +13,49 @@ namespace CSammisRun.Imaging.Test
     [TestFixture]
     public class OneBppImageTest
     {
+        private const int TEST_IMAGE_DIMENSION = 10;
+
+        byte[,] testImageSource = new byte[TEST_IMAGE_DIMENSION,TEST_IMAGE_DIMENSION] {
+            {0,0,1,1,1,1,1,1,0,0},
+            {0,1,0,0,0,0,0,0,0,1},
+            {1,0,0,1,0,0,1,0,0,1},
+            {1,0,0,1,0,0,1,0,0,1},
+            {1,0,0,0,0,0,0,0,0,1},
+            {1,0,0,0,0,0,0,0,0,1},
+            {1,0,1,0,0,0,0,1,0,1},
+            {1,0,0,1,1,1,1,0,0,1},
+            {0,1,0,0,0,0,0,0,1,0},
+            {0,0,1,1,1,1,1,1,0,0}};
+
         [Test]
         public void TestInitFromByteData()
         {
-            byte[,] testImageSource = new byte[10,10] {
-                {0,0,1,1,1,1,1,1,0,0},
-                {0,1,0,0,0,0,0,0,0,1},
-                {1,0,0,1,0,0,1,0,0,1},
-                {1,0,0,1,0,0,1,0,0,1},
-                {1,0,0,0,0,0,0,0,0,1},
-                {1,0,0,0,0,0,0,0,0,1},
-                {1,0,1,0,0,0,0,1,0,1},
-                {1,0,0,1,1,1,1,0,0,1},
-                {0,1,0,0,0,0,0,0,1,0},
-                {0,0,1,1,1,1,1,1,0,0}};
-
             // Original code had a bug in it which reduced the dimensions of the image
             // each time it was constructed (off-by-one with the multidimensional byte access)
             OneBppImage testImage = new OneBppImage(testImageSource);
 
-            Assert.AreEqual(10, testImage.Width);
-            Assert.AreEqual(10, testImage.Height);
+            Assert.AreEqual(TEST_IMAGE_DIMENSION, testImage.Width);
+            Assert.AreEqual(TEST_IMAGE_DIMENSION, testImage.Height);
+        }
+
+        [Test]
+        public void TestCopyConstructor()
+        {
+            OneBppImage testImage = new OneBppImage(testImageSource);
+            OneBppImage copyImage = new OneBppImage(testImage);
+
+            Assert.AreEqual(testImage.Width, copyImage.Width, "Widths are not identical.");
+            Assert.AreEqual(testImage.Height, copyImage.Height, "Heights are not identical.");
+
+            Assert.AreNotSame(testImage.ImageData, copyImage.ImageData, "ImageData for copied image should be different than the original.");
+            for (int x = 0; x < testImage.Width; x++)
+            {
+                for (int y = 0; y < testImage.Height; y++)
+                {
+                    Assert.AreEqual(testImage.ImageData[x,y], copyImage.ImageData[x,y],
+                                    "Pixels are not identical at ({0},{1})", x, y);
+                }
+            }
         }
     }
 }
